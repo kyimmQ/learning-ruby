78917fab38566b39b37cad89f7a8751e075f5bf3
Cac Le <cacphuong.le@gmail.com>
2025-06-29 18:52:29 +0000

    Symbols

---
+++ 1. Basic/3. Symbols.rb
@@ -0,0 +1,18 @@
+instrument_sections = {
+  :drums => 1,
+  :bass => 2,
+  :guitar => 3,
+  :strings => 4,
+  :percussion => 5
+}
+
+puts instrument_sections[:strings]
+
+# short-hand
+instrument_sections = {
+  drums: 1,
+  bass: 2,
+  guitar: 3,
+  strings: 4,
+  percussion: 5
+}
